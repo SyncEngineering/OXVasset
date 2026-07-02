967528215ad7cdc3f17f9baba25915ebd350abd5
abhhiinv <abhinavsajeev2004@gmail.com>
2026-07-02 15:24:28 +0530

    backend port changed from 5000 to 5001

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import app from './src/app.js';
 // Initialize environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 /**
  * Start the server and listen on the configured port.
